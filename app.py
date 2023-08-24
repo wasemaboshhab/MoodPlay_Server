694460587e9bc340baf400797552146dcf8445de
Wasim Shhab <wasem20aboshhab@gmail.com>
2023-08-24 14:08:03 +0300

    the Temporary image path changed

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from deepface import DeepFace
+import os
 
 app = Flask(__name__)
 
@@ -7,27 +8,35 @@ app = Flask(__name__)
 @app.route('/test')
 def test_deep_face_emotions():
     image_path = "images/temp_image.jpg"
-    face_analysis = DeepFace.analyze(img_path=image_path,actions=['emotion'])
+    face_analysis = DeepFace.analyze(img_path=image_path, actions=['emotion'])
     face_emotion = face_analysis[0]["dominant_emotion"]
     return jsonify(mod=face_emotion)
 
 
-@app.route('/analyze_face', methods=['POST'])
+@app.route('/analyze', methods=['POST'])
 def analyze_mood():
     if 'image' not in request.files:
-        return "No image provided", 400
+        return jsonify({'error': 'No image part'})
 
     image_file = request.files['image']
 
     if image_file.filename == '':
-        return "No selected image", 400
+        return jsonify({'error': 'No selected image'})
 
-    image_path = "images/ronaldo.jpg"  # Temporary image path
+    # image_path : to save/image.jpg
+    image_path = "images"
     image_file.save(image_path)
-
     face_analysis = DeepFace.analyze(img_path=image_path, actions=['emotion'])
     face_emotion = face_analysis[0]["dominant_emotion"]
     return jsonify(mod=face_emotion)
+
+    # if image_file:
+    #     filename = os.path.join(app.config['image'], image_file.filename)
+    #     image_file.save(filename)
+
+
+    # image_path = "images/ronaldo.jpg"  # Temporary image path
+    # image_file.save(image_path)
 
 
 if __name__ == '__main__':
